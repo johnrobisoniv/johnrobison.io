ba7acaeb000f5d5647175ed0c7d80fd5deb7ad63
John Robison Hoopes <iv@Johns-MacBook-Pro-2.local>
2018-02-26 22:59:45 -0700

    updates to pseudocode

--- js.js
+++ js.js
@@ -1,9 +1,47 @@
 
+// When you click the h2s the content reveals
+// on hover it gets darker
+// mask in svg ? to stars .. . ?
 
 
+// data
+// mostly links to external things
+// would be cool to go to local data vis with svg
+//
+
+// pop ups / console logs:
+// 'i think it's cool that you use chrome'
+// just so you know i can read your ip address ... my automated lookup has your
+// address as xxx
+
+
+// h2 image
+// click reveals square images vertically aligned 36% apart
+// on click
+// maybe lightbox pops up that you can flip through ...
+// more images  in the set - or link to them visible behind
+// 7 - 10 images / stories
+// one or two triptychs
+// dawn in atlantic
+// croatia
+// uk
+// czech
+// blue ridge
+//
+// rainier
+// st lucia
+// canaria
+// portraits
+// spherical
+// instagram
+
+
+
+
+console.log('you');
 
 d3.select('body')
-  .on('mousemove', function () {
+  .on('click', function () {
     console.log('me!');
   });
 
@@ -16,7 +54,7 @@ d3.selectAll('h2')
 
 
 function reveal() {
-  console.log('sigh');
+//  console.log('sigh');
   d3.selectAll('p')
     // .each(function (d, i) {
     //   console.log(d);
